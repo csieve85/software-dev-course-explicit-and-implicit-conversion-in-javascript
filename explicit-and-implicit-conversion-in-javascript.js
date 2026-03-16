79311adb7bf146eca73c35e1b046b0a688affacc
csieve85 <csieve85@gmail.com>
2026-03-16 11:02:25 -0500

    Completed debugging challenge

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,24 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = Number("5") - 2; /* You have to tell the computer it's a number */
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = false; /* The orginal code had false as a string. */
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
-let totalAge = age + 5;
+let age = "25"; /* We needed to convert the string to a number in order for the machine to output correctly. */
+let totalAge = Number(age) + 5;
 console.log("Total Age: " + totalAge);
+
+/* Implicit type conversion example */
+console.log(8 - undefined); /* This will result in NaN. */
+
+/* Explicit type conversion example */
+let strNumber = "10";
+let num = Number(strNumber);
+console.log(typeof str);
+console.log (typeof num);
+console.log (num + 10);
